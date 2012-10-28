fd9af98c720ac17600b366479a615b5578a0c2f6
Cedric Vidal <cedric@vidal.biz>
2012-10-28 14:05:22 +0100

    Displaying coffees

--- server/bootstrap.js
+++ server/bootstrap.js
@@ -1,3 +1,23 @@
 // if the database is empty on server start, create some sample data.
-Meteor.startup(function () {
+Meteor.startup(function() {
+	if (Coffees.find().count() === 0) {
+		var data = [ {
+			name : "Ristretto",
+			id : "ristretto",
+			price : "0.36",
+		}, {
+			name : "Roma",
+			id : "roma",
+			price : "0.33",
+		}, {
+			name : "Vivalto",
+			id : "vivalto",
+			price : "0.34",
+		}, ];
+
+		for ( var i = 0; i < data.length; i++) {
+			var coffee = data[i];
+			var coffee_id = Coffees.insert(coffee);
+		}
+	}
 });
--- server/publish.js
+++ server/publish.js
@@ -2,22 +2,27 @@
 Lists = new Meteor.Collection("lists");
 
 // Publish complete set of lists to all clients.
-Meteor.publish('lists', function () {
-  return Lists.find();
+Meteor.publish('lists', function() {
+	return Lists.find();
 });
 
+// Coffees
+Coffees = new Meteor.Collection("coffees");
+
+Meteor.publish('coffees', function() {
+	return Coffees.find();
+});
 
 // Todos -- {text: String,
-//           done: Boolean,
-//           tags: [String, ...],
-//           list_id: String,
-//           timestamp: Number}
+// done: Boolean,
+// tags: [String, ...],
+// list_id: String,
+// timestamp: Number}
 Lines = new Meteor.Collection("lines");
 
 // Publish visible items for requested list_id.
-Meteor.publish('lines', function (list_id) {
-  return Lines.find({
-    list_id: list_id
-  });
+Meteor.publish('lines', function(list_id) {
+	return Lines.find({
+		list_id : list_id
+	});
 });
-
